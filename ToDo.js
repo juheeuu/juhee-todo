f7dd6f024d52c8e97626345bae39946223a7d5e6
juhee <sjh5665@gmail.com>
2018-05-26 22:04:39 +0900

    change loading img

--- ToDo.js
+++ ToDo.js
@@ -75,7 +75,9 @@ export default class ToDo extends Component{
                                 <Text style={styles.actionText}>✏️</Text>
                             </View>
                         </TouchableOpacity>
-                        <TouchableOpacity onPressOut={()=> deleteToDo(id)}>
+                        <TouchableOpacity onPressOut={event=>{
+                            event.stopPropagation();
+                            deleteToDo(id);}}>
                             <View style={styles.actionContainer}>
                                 <Text style={styles.actionText}>❌</Text>
                             </View>
@@ -87,7 +89,8 @@ export default class ToDo extends Component{
 
         )
     }
-    _toggleComplete = () => {
+    _toggleComplete = (event) => {
+        event.stopPropagation();
        const { isCompleted, uncompleteToDo, completeToDo, id } = this.props;
        if(isCompleted){
            uncompleteToDo(id)
@@ -95,12 +98,12 @@ export default class ToDo extends Component{
            completeToDo(id)
        }
     };
-    _startEditing = () => {
+    _startEditing = (event) => {
         this.setState({
             isEditing: true,
         });
     };
-    _finishEditing = () => {
+    _finishEditing = (event) => {
         const{ toDoValue} = this.state;
         const {id, updateToDo} = this.props;
         updateToDo(id, toDoValue);
